381ad92437839c729ebc6494e8a3ec9b9e15c295
Andrew Sarracini <andrewsarracini1@gmail.com>
2025-03-30 23:53:34 -0400

    eda2-- better and improved

---
+++ src/eda2.py
@@ -0,0 +1,16 @@
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.feature_selection import mutual_info_classif
+from sklearn.preprocessing import LabelEncoder
+
+# --- EDA: 7 STEPS ---
+# 1. Data Cleaning and Preprocessing
+# 2. Descriptive Stats
+# 3. Univariate Analysis
+# 4. Bivariate Analysis 
+# 5. Multivariate Analysis 
+# 6. Feature Engineering
+# 7. Visualization
+
