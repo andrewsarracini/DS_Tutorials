7de734892c2915f08ed2a8c1150204b4e47bbcec
Andrew Sarracini <andrewsarracini1@gmail.com>
2025-04-16 16:31:58 -0400

    EEG_FILE to simplify

--- src/helper.py
+++ src/helper.py
@@ -383,13 +383,10 @@ def compare_probs(model, calibrated_model, X_val, y_val):
     plt.show()
 
 def get_project_root(): 
-    '''
-    Get the root directory of the project
-    '''
     return os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 
 def resolve_path(*relative_parts):
-    '''
-    Resolve a relative path to an absolute path
-    '''
     return os.path.join(get_project_root(), *relative_parts) 
+
+EEG_FILE = resolve_path('data', 'eeg_hypno.csv')
+LOG_DIR = resolve_path('logs') 
