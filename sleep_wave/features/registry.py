7eeb12e4ff4a6254bab8912d8ad8534c19e0724b
Andrew Sarracini <andrewsarracini1@gmail.com>
2025-04-18 03:22:59 -0400

    completing removal feat_temporal_bandpower()

--- sleep_wave/features/registry.py
+++ sleep_wave/features/registry.py
@@ -1,6 +1,6 @@
 # sleep_wave/features/registry.py 
 
-from sleep_wave.features.builders import feat_bandpower_lags, feat_temporal_bandpower_t1
+from sleep_wave.features.builders import feat_bandpower_lags
 
 def register_all_features():
     '''
@@ -8,13 +8,8 @@ def register_all_features():
     '''
     return [
         {
-            'name': '+- temporal bandpower',
-            'func': feat_temporal_bandpower_t1,
-            'notes': 'Adds t-1 and t+1 EEG bandpower features'
-        },
-        {
             'name': 'EEG band lag-1 and lag-2',
             'func': feat_bandpower_lags,
             'notes': 'Adds 1- and 2-epoch lag feats for each band'
         }
-    ]
+    ] # Add more features here! 
