7417ab82e8ce40e0e0c2571372818bb89d665f0e
Andrew Sarracini <andrewsarracini1@gmail.com>
2025-05-01 00:28:13 -0400

    SleepLSTM copmlete and documented

--- src/models/lstm_model.py
+++ src/models/lstm_model.py
@@ -1,7 +1,11 @@
 import torch 
 import torch.nn as nn 
 
-
+# input size: # feats / timestep (bands) + engineered feats
+# hidden_size: LSTM units 
+# num_layers: stacked LSTM layers
+# num_classes: 2 for binary, 5 for multiclass (N1, N2, N3, REM, Wake) 
+# dropout: used between LSTM layers (if > 1) 
 class SleepLSTM(nn.Module):
     def __init__(self, input_size, hidden_size=64, num_layers=1, num_classes=5, dropout=0.0):
         super(SleepLSTM, self).__init__()
@@ -10,12 +14,18 @@ class SleepLSTM(nn.Module):
             input_size=input_size, 
             hidden_size=hidden_size,
             num_layers=num_layers,
-            batch_first=True, 
+            batch_first=True, # makes input shape (batch, seq_len, input_size)
             dropout=dropout if num_layers > 1 else 0.0
         )
+        # final layer
+        # transforms last hidden state -> logits over class scores
         self.fc = nn.Linear(hidden_size, num_classes) 
 
-    def forwar(self, x):
+    # output sequence of hidden states (discard all but the last!) 
+    # hn, cn: hidden cell states 
+    # Take last timestep output; pass it through fc() 
+    # `logits` produced... feed into CrossEntropyLoss 
+    def forward(self, x):
         # x: (batch, seq_len, input_size) 
         output, (hn, cn) = self.lstm(x)
         last_hidden = output[:,-1, :] # Take last time step
