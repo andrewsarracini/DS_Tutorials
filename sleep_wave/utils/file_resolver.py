1d395275ba3be479ebe3e5606c6f8706ffdfeb6f
Andrew Sarracini <andrewsarracini1@gmail.com>
2025-06-15 21:33:09 -0400

    fixed subject-matching logic

--- sleep_wave/utils/file_resolver.py
+++ sleep_wave/utils/file_resolver.py
@@ -4,10 +4,28 @@ from pathlib import Path
 from src.paths import DATA_DIR
 
 def get_edf_paths(subject_id: str):
-    subject_tag = f"ST{subject_id}J"
-    psg_path = next(DATA_DIR.glob(f"{subject_tag}*-PSG.edf"), None)
-    hypnogram_path = next(DATA_DIR.glob(f"{subject_tag}*-Hypnogram.edf"), None)
-    if not psg_path:
-        raise FileNotFoundError(f"PSG file for subject {subject_id} not found.")
-    return psg_path, hypnogram_path
+    '''
+    Given a subject ID (ex. 7011), returns matching PSG and Hypnogram EDF paths
+    '''
 
+    subject_tag = f'ST{subject_id}'
+
+    # Match PSG
+    psg_candidates = list(DATA_DIR.glob(f'{subject_tag}*-PSG.edf'))
+    if not psg_candidates:
+        raise FileNotFoundError(f'PSG file for s{subject_id} not found')
+    if len(psg_candidates) > 1:
+        print(f'[WARN] Multiple PSG files found for s{subject_id}, using first: {psg_candidates[0]}')
+    psg_path = psg_candidates[0] 
+
+    # Match Hypno
+    hypnogram_candidates = list(DATA_DIR.glob(f'{subject_tag}*-Hypnogram.edf'))
+    if not hypnogram_candidates:
+        print(f'[WARN] No Hypnogram found for subject s{subject_id}. No labels applied')
+        hypnogram_path = None
+    else:
+        hypnogram_path = hypnogram_candidates[0]
+        if len(hypnogram_candidates) > 1: 
+            print(f'[WARN] Multiple Hypnogram files found for s{subject_id}, using first: {hypnogram_candidates[0]}')
+        
+    return psg_path, hypnogram_path
