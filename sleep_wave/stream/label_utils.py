f57e2dfb1cfb36a2c96de0b85c0921ffa2dc2086
Andrew Sarracini <andrewsarracini1@gmail.com>
2025-06-17 23:56:15 -0400

    label_utils update: strip()

--- sleep_wave/stream/label_utils.py
+++ sleep_wave/stream/label_utils.py
@@ -3,8 +3,10 @@
 def extract_epoch_labels(annotations, epoch_len, total_duration, stage_map):
     n_epochs = total_duration // epoch_len
     labels = ['UNKNOWN'] * n_epochs
+
     for ann in annotations:
-        label = stage_map.get(ann['description'], None)
+        desc = ann['description'].strip().lower()
+        label = stage_map.get(desc, None)
         if label is None:
             continue
         start = int(ann['onset']) // epoch_len
